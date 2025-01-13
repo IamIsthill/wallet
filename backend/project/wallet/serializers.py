126a99c692083f244fd52f6e16c854620da56da3
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2025-01-13 21:35:35 +0800

    Added serializer

---
+++ backend/project/wallet/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Account
+
+
+class AccountSeriliazer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+        read_only_fields = ['user']
