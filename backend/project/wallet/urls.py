ee0200ce0d507d3302b8467998e158d3635c2a06
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2025-01-18 23:35:46 +0800

    Added delete and update wallet

--- backend/project/wallet/urls.py
+++ backend/project/wallet/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import AccountListCreate
+from .views import AccountListCreate, AccountRetrieveUpdateDestroy
+
+BASE_PATH = "wallet-account"
 
 urlpatterns = [
     path(
-        "wallet-account/create/",
+        f"{BASE_PATH}/create/",
         AccountListCreate.as_view(),
         name="create-wallet-account",
+    ),
+    path(
+        f"{BASE_PATH}/<int:pk>/",
+        AccountRetrieveUpdateDestroy.as_view(),
+        name="wallet-account",
     ),
 ]
--- backend/project/wallet/views.py
+++ backend/project/wallet/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Account
 from .serializers import AccountSeriliazer
@@ -22,3 +22,25 @@ class AccountListCreate(generics.ListCreateAPIView):
             serializer.save(user=user)
         else:
             print(serializer.errors)
+
+
+class AccountRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = AccountSeriliazer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        account = Account.objects.filter(id=self.kwargs["pk"], user=user)
+        return account
+
+    def perform_update(self, serializer):
+        user = self.request.user
+        if serializer.is_valid():
+            serializer.save(user=user)
+        else:
+            print(serializer.errors)
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+        if user == instance.user:
+            instance.delete()
